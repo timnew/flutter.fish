6d216c7eb56b71952323e4432bb0fbd420786659
Tim <timnew.wti@gmail.com>
2021-03-12 16:06:33 +1100

    First version

---
+++ README.md
@@ -0,0 +1,19 @@
+# flutter.fish
+
+Plugin for *fish shell* to integrate flutter into efficiency-seeking workflows.
+
+## Prerequisite
+
+Install flutter and dart SDK
+
+## Install
+
+With [fisherman]
+
+```fish
+$ fisher add timnew/flutter.fish
+```
+
+Hope you have a _fast_ swim!
+
+[fisherman]: https://github.com/fisherman/fisherman
---
+++ conf.d/flutter.fish
@@ -0,0 +1,11 @@
+abbr -a -U -- fpg 'flutter pub get'
+
+abbr -a -U -- fr 'flutter run'
+abbr -a -U -- frr 'flutter run --release'
+
+abbr -a -U -- fbrc 'flutter pub run build_runner clean'
+abbr -a -U -- fbrb 'flutter pub run build_runner build'
+abbr -a -U -- fbrbd 'flutter pub run build_runner build --delete-conflicting-outputs'
+
+abbr -a -U -- fa 'flutter analyze'
+abbr -a -U -- fpp 'flutter pub publish'
